73cf7e453c2c91e3f67abfa1a048035559982ac1
79283 <79283@LAPTOP-TT6N6EBU>
2023-12-12 16:35:03 +0300

    collection Day 1

---
+++ Police/Base.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+using System.Diagnostics;
+
+namespace Police
+{
+    internal class Base
+    {
+       private Dictionary<LicencePlate, List<Crime>> police_base;
+        const string delimiter = "\n------------------\n";
+        public Base(Dictionary<LicencePlate,List <Crime>>police_base)
+        {
+            this.police_base=new Dictionary<LicencePlate, List<Crime>>(police_base);
+        }
+        public void Print()
+        {
+            foreach (KeyValuePair<LicencePlate, List<Crime>> i in police_base)
+            {
+                Console.WriteLine($"{i.Key}:\n");
+                foreach (Crime j in i.Value)
+                {
+                    Console.WriteLine($"\t{j}");
+                }
+                Console.WriteLine(delimiter);
+            }
+        }
+        public void  Save(string filename)
+        {
+            StreamWriter sw = new StreamWriter(filename);
+            foreach(KeyValuePair<LicencePlate,List<Crime>> i in police_base)
+            {
+                sw.Write(i.Key+":");
+                foreach(Crime j in i.Value)
+                {
+                    sw.Write(j+",");
+                }
+               sw.WriteLine(); 
+            }
+            sw.Close();
+            System.Diagnostics.Process.Start("notepad", filename);
+        }
+    }
+}
---
+++ Police/Violations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Police
+{
+     internal class Violations
+    {
+        public static readonly Dictionary<int, string> list = new Dictionary<int, string>()
+        {
+            [0]="N/A",
+            [1]="Парковка в неполложенном месте",
+            [2]="Ремень безопасности",
+            [3]="Пересечение сплошной",
+            [4]="Превышение скорости",
+            [5]="Проезд на красный",
+            [6]="Езда в не трезвом состоянии",
+            [7]="Оскорбление офицера",
+            [8]="Сопротивление при аресте",
+            [9]="Не пропустил пешехода",
+            [10]="Езда по встречной полосе",
+            [11]="Нарушение переезда ж/д путей",
+            [12]="Оставление места ДТП"
+        };
+        
+    }
+}
